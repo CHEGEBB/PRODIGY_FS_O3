48e8c57be0d084f57153a941fdb10f4e7646d419
CHEGEBB <chegephil24@gmail.com>
2024-08-03 12:27:41 +0300

    added controllers

---
+++ backend/controllers/auth.contollers.js
@@ -0,0 +1,14 @@
+
+export const login = (req, res) =>{
+    console.log('Login user');
+
+};
+
+export const signup = (req, res) =>{
+    console.log('Signup user');
+
+};
+
+export const logout = (req, res) =>{
+    console.log('Logout user');
+}
