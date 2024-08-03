2fe4574365c2e99c4fd6eab9675bfbd717391d40
CHEGEBB <chegephil24@gmail.com>
2024-08-03 17:35:31 +0300

    chore: Update auth.model.js to include timestamps
    This commit updates the auth.model.js file to include the {timestamps: true} option in the userSchema. This will automatically add createdAt and updatedAt fields to the user documents, allowing for easy tracking of when the documents were created and last updated.
    
    Note: This message is based on the provided code changes and recent repository commits.

---
+++ backend/models/convoModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const conversationSchema = new mongoose.Schema({
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }],
+    messages: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message',
+        default:[]
+    }]
+},{timestamps: true});
+
+export default mongoose.model('Conversation', conversationSchema);
