a430eb2465dd3ed2d930bfe7bfc3ad824d7f3847
CHEGEBB <chegephil24@gmail.com>
2024-08-03 12:24:32 +0300

    chore: Update .gitignore and server.js
    This commit updates the .gitignore file to include the .env file and adds import statements to the server.js file for using ES modules. It also sets up the server to listen on the port specified in the environment variable. Additionally, it adds a new route for handling authentication requests.

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/login');
+
+router.get('/signup');
+
+router.get('/logout');
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
-const express = require ('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-app.listen(5000 , (err, res) =>{
+const PORT =process.env.port
+
+app.listen(PORT, (err, res) =>{
     if(err) throw err;
     console.log('Server running on port 5000');
 })
+
+app.use('/api/auth', authRoutes);
