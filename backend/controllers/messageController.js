3414aaf1134203398a3f158e4fabfdcfe5123729
CHEGEBB <chegephil24@gmail.com>
2024-08-04 12:05:13 +0300

    chore: Add logic to handle sending messages in messageController.js

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -4,6 +4,28 @@ try {
     const {message}= req.body;
     const {id} = req.params;
     const senderId = req.user._id;
+
+  let conversation =  await Conversation.findOne({
+        participants:{
+            $all:[senderId,id]
+        }
+    });
+    
+    if(!conversation){
+        conversation = await Conversation.create({
+            participants:[senderId,id]
+        });
+    }
+
+    const newMessage = new Message({
+        senderId,
+        receiverId,
+        message,
+    });
+		if (newMessage) {
+			conversation.messages.push(newMessage._id);
+		}
+
     
 } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' });
