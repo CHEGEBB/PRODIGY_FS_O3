816b9bb7c0deefaa26bbef4c7b8a9bf262dd2a1f
CHEGEBB <chegephil24@gmail.com>
2024-08-05 19:41:30 +0300

    controllers

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import { sendMessage } from '../controllers/messageController.js';
-import protectRoute from '../middleware/protectRoute.js';
+import express from "express";
+import { getMessages, sendMessage } from "../controllers/messageController.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-// TODO: Implement routes for creating, reading, updating, and deleting messages
-
-router.post('/send/:id',protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
