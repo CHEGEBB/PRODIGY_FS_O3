16a5087345bd6ad8e7e25a91e1ee025db92c163f
CHEGEBB <chegephil24@gmail.com>
2024-08-05 19:51:02 +0300

    new

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import messageRoutes from './routes/messageRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 // Load environment variables
 dotenv.config();
@@ -37,6 +38,14 @@ app.use('/api/auth', authRoutes);
 
 //message routes
 app.use('/api/messages', messageRoutes);
+app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 
 // Start server
--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -1,75 +1,50 @@
+// Import required modules
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
+// Initialize Express app
 const app = express();
 
+// Create HTTP server and Socket.io instance
 const server = http.createServer(app);
 const io = new Server(server, {
-	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST"],
-	},
+    cors: {
+        origin: ["http://localhost:3000"], // Allow connections from this origin
+        methods: ["GET", "POST"], // Allow these HTTP methods
+    },
 });
 
-export const getReceiverSocketId = (receiverId) => {
-	return userSocketMap[receiverId];
-};
-
+// Object to store user ID to socket ID mappings
 const userSocketMap = {}; // {userId: socketId}
 
-io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
-
-	const userId = socket.handshake.query.userId;
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
-
-	// io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
-		console.log("user disconnected", socket.id);
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
-});
-
-export { app, io, server };import { Server } from "socket.io";
-import http from "http";
-import express from "express";
-
-const app = express();
-
-const server = http.createServer(app);
-const io = new Server(server, {
-	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST"],
-	},
-});
-
+// Function to get a user's socket ID by their user ID
 export const getReceiverSocketId = (receiverId) => {
-	return userSocketMap[receiverId];
+    return userSocketMap[receiverId];
 };
 
-const userSocketMap = {}; // {userId: socketId}
-
+// Handle new socket connections
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
+    console.log("a user connected", socket.id);
 
-	const userId = socket.handshake.query.userId;
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
+    // Get user ID from the connection query parameters
+    const userId = socket.handshake.query.userId;
+    
+    // If userId is valid, store the mapping
+    if (userId !== "undefined") userSocketMap[userId] = socket.id;
 
+    // Broadcast the list of online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
-		console.log("user disconnected", socket.id);
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+    // Handle socket disconnection
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+        // Remove the user from the socket map
+        delete userSocketMap[userId];
+        // Update all clients with the new list of online users
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
 });
 
+// Export the app, io, and server for use in other parts of the application
 export { app, io, server };
